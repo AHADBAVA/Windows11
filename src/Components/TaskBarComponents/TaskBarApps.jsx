adacb8b7cc8f2e7067a8817d034fbd2e71454742
AHADBAVA <ahaddesgins@gmail.com>
2024-04-30 11:38:01 +0530

    Files Created

---
+++ src/Components/TaskBarComponents/TaskBarApps.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskBarApps = () => {
+  return (
+    <div>
+      Using Apps Or Pinned Apps
+    </div>
+  )
+}
+
+export default TaskBarApps
---
+++ src/Components/TaskBarComponents/TaskBarSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskBarSearch = () => {
+  return (
+    <div>
+      Search Bar 
+    </div>
+  )
+}
+
+export default TaskBarSearch
